47523be520e7afa62db3d15526dda5464ce63a73
Justin Chang <justin.p.chang@gmail.com>
2023-04-02 14:53:50 -0400

    Improve Chat styling

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Message from "./Message";
 
 export default function Chat() {
   const [inputValue, setInputValue] = useState("");
 
+  const messagesBottomRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesBottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+
   return (
-    <div className="border border-slate-300 flex flex-col grow min-h-0">
-      <div className="divide-y divide-slate-300  overflow-y-auto">
+    <div className="border border-slate-300 grow min-h-0 overflow-y-auto relative">
+      <div className="divide-y divide-slate-300 mb-24 overflow-y-auto">
         <Message isFromUser={true}>
           This is a test message from the user.
         </Message>
@@ -37,15 +47,18 @@ export default function Chat() {
           Or a third.
         </Message>
       </div>
-      <div className="flex justify-between p-3 m-3 border border-slate-200 rounded-md shadow-md">
-        <input
-          className="grow focus:outline-none"
-          type="text"
-          placeholder="Send a message..."
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button onClick={() => setInputValue("")}>Send</button>
+      <div ref={messagesBottomRef} />
+      <div className="fixed bottom-6 flex items-center w-full">
+        <div className="w-[90vw] mx-auto flex justify-between p-3 m-3 bg-white border border-slate-200 rounded-md shadow-2xl">
+          <input
+            className="grow focus:outline-none"
+            type="text"
+            placeholder="Send a message..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button onClick={() => setInputValue("")}>Send</button>
+        </div>
       </div>
     </div>
   );
--- src/components/Chat/Message.tsx
+++ src/components/Chat/Message.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { ComputerDesktopIcon } from "@heroicons/react/24/outline";
 
 interface Props {
   isFromUser: boolean;
@@ -10,9 +11,15 @@ export default function Message({ isFromUser, children }: Props) {
     <div
       className={`${isFromUser ? "bg-white" : "bg-slate-50"} flex gap-3 p-6`}
     >
-      <div className="h-10 w-10 bg-slate-600 text-sm text-white flex items-center justify-center">
-        {isFromUser ? "You" : "AI"}
-      </div>
+      {isFromUser ? (
+        <div className="h-10 w-10 bg-cyan-700 text-sm text-white flex items-center justify-center">
+          You
+        </div>
+      ) : (
+        <div className="h-10 w-10 bg-slate-600 text-white flex items-center justify-center">
+          <ComputerDesktopIcon className="h-6 w-6" />
+        </div>
+      )}
       <div className="mt-1 text-slate-600 text-lg">{children}</div>
     </div>
   );
