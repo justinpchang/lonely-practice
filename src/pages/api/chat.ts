0ae6a7a5d666804b37bceba58a5b57a510708623
Justin Chang <justin.p.chang@gmail.com>
2023-04-02 16:21:18 -0400

    Add basic chat gpt endpoint

---
+++ src/pages/api/chat.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ChatOpenAI } from "langchain/chat_models";
+import {
+  ChatPromptTemplate,
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+} from "langchain/prompts";
+import { ConversationChain } from "langchain/chains";
+import { BufferMemory } from "langchain/memory";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const chat = new ChatOpenAI({ temperature: 0.7 });
+  const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      "Pretend you are a friendly Italian conversation practice tutor. Only reply with primary-school level Italian."
+    ),
+    HumanMessagePromptTemplate.fromTemplate("{input}"),
+  ]);
+  const chain = new ConversationChain({
+    memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
+    prompt: chatPrompt,
+    llm: chat,
+  });
+
+  const response = await chain.call({
+    input: req.query.input,
+  });
+
+  res.status(200).json(response);
+}
