9fde0082d07f82b1358651de79be992685d6e8d5
Justin Chang <justin.p.chang@gmail.com>
2023-05-09 18:51:21 -0400

    Remove debug endpoint logic

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,3 @@
-import { Database } from "@/types/supabase";
-import { ConversationLog } from "@/utils/conversationLog";
-import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -11,17 +8,5 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const supabaseServerClient = createServerSupabaseClient<Database>({
-    req,
-    res,
-  });
-  const {
-    data: { user },
-  } = await supabaseServerClient.auth.getUser();
-  const userId = user!.id;
-
-  const log = new ConversationLog(userId);
-  console.log(await log.getConversation({ limit: 10 }));
-
   res.status(200).json({ name: "John Doe" });
 }
